e7b0cbf66de76b54cf4a2788b52fe271651b11ef
lenin fernandes <leninfernandes81@gmail.com>
2020-01-17 15:23:35 +0530

    1st commit

---
+++ experiment/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace experiment
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.newButton = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openButton
+            // 
+            this.openButton.AutoSize = true;
+            this.openButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.openButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.openButton.Location = new System.Drawing.Point(41, 0);
+            this.openButton.Margin = new System.Windows.Forms.Padding(0);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(45, 25);
+            this.openButton.TabIndex = 1;
+            this.openButton.Text = "Open";
+            this.openButton.UseVisualStyleBackColor = true;
+            this.openButton.Click += new System.EventHandler(this.openButton_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.AutoSize = true;
+            this.saveButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.saveButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.saveButton.Location = new System.Drawing.Point(86, 0);
+            this.saveButton.Margin = new System.Windows.Forms.Padding(0);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(44, 25);
+            this.saveButton.TabIndex = 2;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 29;
+            this.listBox1.Location = new System.Drawing.Point(3, 28);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.ScrollAlwaysVisible = true;
+            this.listBox1.Size = new System.Drawing.Size(264, 698);
+            this.listBox1.TabIndex = 4;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // newButton
+            // 
+            this.newButton.AutoSize = true;
+            this.newButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.newButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.newButton.Location = new System.Drawing.Point(0, 0);
+            this.newButton.Margin = new System.Windows.Forms.Padding(0);
+            this.newButton.Name = "newButton";
+            this.newButton.Size = new System.Drawing.Size(41, 25);
+            this.newButton.TabIndex = 5;
+            this.newButton.Text = "New";
+            this.newButton.UseVisualStyleBackColor = true;
+            this.newButton.Click += new System.EventHandler(this.newButton_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoSize = true;
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 80F));
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.richTextBox1, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.listBox1, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1350, 729);
+            this.tableLayoutPanel1.TabIndex = 6;
+            this.tableLayoutPanel1.TabStop = true;
+            // 
+            // panel1
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.panel1, 2);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.newButton);
+            this.panel1.Controls.Add(this.saveButton);
+            this.panel1.Controls.Add(this.openButton);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(222, 25);
+            this.panel1.TabIndex = 7;
+            // 
+            // button2
+            // 
+            this.button2.AutoSize = true;
+            this.button2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(130, 0);
+            this.button2.Margin = new System.Windows.Forms.Padding(0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(50, 25);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Delete";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.EnableAutoDragDrop = true;
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(273, 28);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(1074, 698);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1350, 729);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button openButton;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button newButton;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ experiment/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace experiment
+{
+    public partial class Form1 : Form
+    {
+        public static bool autosave = false;
+        public static System.String actFile;
+        public static System.String actFileName;
+        public Form1()
+        {
+            InitializeComponent();
+            refresh_list();
+            
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            save_data();
+        }
+        private void save_data()
+        {
+            if (autosave == true)
+            {
+                using (TextWriter writer = File.CreateText(actFile))
+                {
+                    writer.WriteLine(richTextBox1.Text);
+                }
+                Console.WriteLine("saved");
+                //rename_file();
+            }
+            refresh_list();
+        }
+        private void openButton_Click(object sender, EventArgs e)
+        {
+            open_file();
+        }
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            //using (TextWriter writer = File.CreateText("c:\\Test\\" + actFile + ".txt"))
+            //{
+            //    writer.WriteLine(richTextBox1.Text);
+            //}
+        }
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Console.WriteLine("listbox select Index Changed");
+            //actFile = listBox1.Items[listBox1.SelectedIndex].ToString();
+            actFile = "c:\\Test\\" + listBox1.Items[listBox1.SelectedIndex].ToString() + ".txt";
+            actFileName = listBox1.Items[listBox1.SelectedIndex].ToString() + ".txt";
+            open_file();
+        }
+        private void listBox1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("listbox_click");
+            actFile = "c:\\Test\\" + listBox1.Items[listBox1.SelectedIndex].ToString() + ".txt";
+            actFileName = listBox1.Items[listBox1.SelectedIndex].ToString() + ".txt";
+        }
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+        public void rename_file()
+        {
+            //to get first line of file
+            string[] lines = File.ReadAllLines(actFile);
+            Console.Write(lines[0]);
+
+            //to get original file name
+            DirectoryInfo d = new DirectoryInfo(@"C:\Test");
+            FileInfo[] infos = d.GetFiles();
+            foreach (FileInfo f in infos)
+            {
+                Console.WriteLine("Actfilename="+actFileName);
+                Console.WriteLine("f.fullname=" + f.FullName);
+                File.Move(f.FullName, f.FullName.Replace(actFileName,lines[0]));
+
+            }
+            actFileName = lines[0];
+            actFile= "c:\\Test\\" + actFileName+ ".txt";
+
+        }
+        public  void refresh_list()
+        {
+            listBox1.Items.Clear();
+            string[] fileList;
+            fileList = new string[20];
+            ListViewItem list1 = new ListViewItem();
+            DirectoryInfo d = new DirectoryInfo(@"c:\\Test");//Assuming Test is your Folder
+            FileInfo[] Files = d.GetFiles("*.txt"); //Getting Text files
+            foreach (FileInfo file in Files)
+            {
+                int i = 0;
+                fileList[i] = file.Name;
+                listBox1.Items.Add(Path.GetFileNameWithoutExtension(file.Name));
+                i++;
+            }
+        }
+        public void open_file()
+        {
+            autosave = false;
+            Console.WriteLine("open_click");
+            //string path = "c:\\Test\\" +actFile+".txt";
+            using (TextReader tr = File.OpenText(actFile))
+            {
+                //Console.WriteLine(tr.ReadToEnd());
+                richTextBox1.Text = (tr.ReadToEnd());
+            }
+            Console.WriteLine("open_click_end");
+            autosave = true;
+        }
+
+        
+
+        private void newButton_Click(object sender, EventArgs e)
+        {
+
+        }
+        public void new_note()
+        {
+            
+            actFile = "c:\\Test\\Untitled.txt";
+            TextWriter writer = File.CreateText(actFile);
+        }
+    }
+}
